43eb77b5d94355f9928df6c03546850226959639
Woolam <wlam135@naver.com>
2022-02-06 21:43:36 +0900

    유저 회원가입, 로그인 구현

--- src/main/java/com/hwai/backend/controller/UsersController.java
+++ src/main/java/com/hwai/backend/controller/UsersController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/users")
-public class UsersApiController {
+public class UsersController {
 
     private final UsersService usersService;
 
