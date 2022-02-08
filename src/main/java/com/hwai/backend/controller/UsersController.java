5cb31b6070a7642b9277d5268eae7d88b8bf7fb6
Woolam <wlam135@naver.com>
2022-02-08 16:36:50 +0900

    프론트와 서버 통신 테스트

--- src/main/java/com/hwai/backend/controller/UsersController.java
+++ src/main/java/com/hwai/backend/controller/UsersController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/v1/users")
+@RequestMapping("/api/v1/users")
 public class UsersController {
 
     private final UsersService usersService;
