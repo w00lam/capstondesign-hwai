86aaf5aa05506ec5082b9e4a7700c5e6578d001a
Woolam <wlam135@naver.com>
2022-02-11 15:11:18 +0900

    대출 기능 추가

--- src/main/java/com/hwai/backend/controller/book/BookController.java
+++ src/main/java/com/hwai/backend/controller/book/BookController.java
@@ -1,4 +1,4 @@
-package com.hwai.backend.controller;
+package com.hwai.backend.controller.book;
 
 import com.hwai.backend.controller.book.dto.LendRequestDto;
 import com.hwai.backend.domain.message.Message;
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
-@RestController
 @RequestMapping("/api/v1/books")
+@RestController
 public class BookController {
 
     private final BookService bookService;
