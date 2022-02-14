73324aee1bb5375b3ba9f0131ad8907b0086bb5b
Woolam <wlam135@naver.com>
2022-02-14 13:05:13 +0900

    책 테이블, 책 저장 수정

--- src/main/java/com/hwai/backend/book/domain/BookRepository.java
+++ src/main/java/com/hwai/backend/book/domain/BookRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    @Query(value = "SELECT * FROM book b INNER JOIN category c ON b.shelf = c.shelf WHERE shelf != current", nativeQuery = true)
+    @Query(value =
+            "SELECT * FROM book b INNER JOIN category c ON b.category_id = c.id WHERE b.current != c.shelf"
+            , nativeQuery = true)
     List<Book> findCheck();
 
-    @Query(value = "SELECT * FROM book b WHERE b.user_id IS NULL", nativeQuery = true)
+    @Query(value =
+            "SELECT * FROM book b WHERE b.user_id IS NULL"
+            , nativeQuery = true)
     List<Book> findLendAble();
 }
