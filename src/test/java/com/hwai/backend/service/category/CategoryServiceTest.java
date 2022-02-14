c2e3ac055f92be773fbe3f738ba264f578945a36
Woolam <wlam135@naver.com>
2022-02-14 16:36:32 +0900

    대출 기능 수정

--- src/test/java/com/hwai/backend/service/category/CategoryServiceTest.java
+++ src/test/java/com/hwai/backend/service/category/CategoryServiceTest.java
@@ -1,15 +1,11 @@
 package com.hwai.backend.service.category;
 
-import com.hwai.backend.book.domain.BookRepository;
-import com.hwai.backend.book.service.BookService;
 import com.hwai.backend.category.controller.dto.AddRequestDto;
 import com.hwai.backend.category.domain.Category;
 import com.hwai.backend.category.domain.CategoryRepository;
 import com.hwai.backend.category.service.CategoryService;
+import com.hwai.backend.common.exception.BadRequestException;
 import com.hwai.backend.common.message.Message;
-import com.hwai.backend.user.domian.UserRepository;
-import com.hwai.backend.user.service.UserService;
-import org.junit.After;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.runner.RunWith;
@@ -18,40 +14,40 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CategoryServiceTest {
     @Autowired
-    private BookRepository bookRepository;
-
-    @Autowired
     private CategoryRepository categoryRepository;
-
-    @Autowired
-    private BookService bookService;
 
     @Autowired
     private CategoryService categoryService;
 
     @AfterEach
     void cleanUp() {
-        bookRepository.deleteAll();
         categoryRepository.deleteAll();
     }
 
     @Test
-    public void 카테고리_추가() {
+    public void 카테고리_추가_실패() {
         //given
+        Category category = Category.builder()
+                .shelf("1")
+                .genre("genre")
+                .build();
+        Category save = categoryRepository.save(category);
+
         AddRequestDto addRequestDto = AddRequestDto.builder()
                 .genre("genre")
-                .shelf("shelf")
+                .shelf("1")
                 .build();
 
         //when
-        Message message = categoryService.add(addRequestDto);
-
         //then
-        assertThat(message.getMessage()).isEqualTo("카테고리 저장 성공");
+        assertThatThrownBy(() -> categoryService.add(addRequestDto))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessage("장르 중복입니다.");
     }
 }
