efdc86e6fb15195e396227a2321e4bd6917d7a2e
Woolam <wlam135@naver.com>
2022-02-25 12:08:03 +0900

    관리자 체크리스트 수정

--- src/main/java/com/hwai/backend/book/controller/dto/ChecklistResponseDto.java
+++ src/main/java/com/hwai/backend/book/controller/dto/ChecklistResponseDto.java
@@ -13,11 +13,13 @@ public class ChecklistResponseDto {
     private String title;
     private LocalDate due_date;
     private String current;
+    private String shelf;
 
     @Builder
     public ChecklistResponseDto(Book book) {
         this.title = book.getTitle();
         this.due_date = book.getDue_date();
         this.current = book.getCurrent();
+        this.shelf = book.getCategory().getShelf();
     }
 }
